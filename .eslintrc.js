c20fe69ad9eb161aaf23ac5655ba9b31673de676
Patrick <patrick.vuscan@gmail.com>
2020-11-01 10:16:15 -0500

    chore: updated ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es6: true,
@@ -6,7 +7,9 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'airbnb',
+    'plugin:jsx-a11y/recommended',
   ],
   globals: {
     Atomics: 'readonly',
@@ -21,10 +24,40 @@ module.exports = {
   },
   plugins: [
     'react',
+    'react-hooks',
+    'import',
+    'import-newlines',
+    'jsx-a11y',
   ],
   rules: {
     indent: ['error', 2],
+    'import-newlines/enforce': ['error', {
+      items: 3,
+      'max-len': 100,
+      semi: false,
+    }],
     'no-multi-spaces': ['error'],
     'react/style-prop-object': ['off'],
+    'no-console': 'off',
+    'no-use-before-define': 'off',
+    'object-property-newline': ['error', {
+      allowAllPropertiesOnSameLine: true,
+    }],
+    'react/prop-types': ['off'],
+    'react/jsx-fragments': ['error', 'syntax'],
+    'react/jsx-first-prop-new-line': ['error', 'multiline'],
+    'react/jsx-closing-tag-location': ['error'],
+    'react/jsx-max-props-per-line': ['error', {
+      maximum: 1,
+    }],
+    'react/jsx-one-expression-per-line': ['error', {
+      allow: 'literal',
+    }],
+    'react/jsx-tag-spacing': ['error', {
+      closingSlash: 'never',
+      beforeSelfClosing: 'always',
+      afterOpening: 'never',
+      beforeClosing: 'never',
+    }],
   },
 };
