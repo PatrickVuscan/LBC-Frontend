780da75c0b81bdb39156768dbae455761ba6027d
Danny <danisnotonfire.chen@mail.utoronto.ca>
2020-11-14 21:10:20 -0500

    I added York Regional Police resources.

--- screens/Articles.jsx
+++ screens/Articles.jsx
@@ -30,7 +30,7 @@ const Articles = (props) => (
             {"\n"}
           </Text>
           <Text>
-            For non-emergency hate crimes, call the Non-Emergency Line at 905-453-3311.
+            For non-emergency hate crimes, report the Non-Emergency Line at 905-453-3311.
             {"\n"}
             or file a hate crime report here:
             {"\n"}
@@ -67,13 +67,46 @@ const Articles = (props) => (
             {"\n"}
           </Text>
           <Text>
-            For non-emergency hate crimes, call the Non-Emergency Line at 808-2222.
+            For non-emergency hate crimes, report the Non-Emergency Line at 808-2222.
             {"\n"}
           </Text>
           <Text>
-            To learn more about hate crimes, call the Hate Crime Unit at 416-808-3500.
+            To learn more about hate crimes, call the Hate Crime Unit at 416-808-3500
             {"\n"}
             or visit the Hate Crime Unit site here: http://www.torontopolice.on.ca/crimeprevention/hatecrime.php
+            {"\n"}
+          </Text>
+          <Button>
+            <Text
+              onPress={() => {
+                props.navigation.navigate('Resources');
+              }}
+            >
+              Click Me!
+            </Text>
+          </Button>
+          <Text>
+            {"\n"}
+          </Text>
+        </View>
+
+        <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>
+            York Regional Police
+            { "\n"}
+          </Text>
+          <Text>
+            For an emergency, call 9-1-1.
+            {"\n"}
+          </Text>
+          <Text>
+            For non-emergency hate crimes, report the Non-Emergency Line at 1-866-876-5423.
+            {"\n"}
+          </Text>
+          <Text>
+            To learn more about hate crimes, call the Hate Crime Hotline at 1-877-354-4283
+            {"\n"}
+            or email the Hate Crime Unit site here at hatecrime@yrp.ca.
             {"\n"}
           </Text>
         </View>
