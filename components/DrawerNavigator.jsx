ed156e8580540183d7b998741086194c2f47e715
Danny <danisnotonfire.chen@mail.utoronto.ca>
2020-12-04 20:01:07 -0500

    Revert "feat: I added an option for users to log out of their account."
    This reverts commit c43a13e179a3887fbad027054d9ae82c9cd42ed0.

--- components/DrawerNavigator.jsx
+++ components/DrawerNavigator.jsx
@@ -9,7 +9,6 @@ import { View, Image } from 'react-native';
 import Timeline from '../screens/Timeline';
 import { colours } from '../theme/theme';
 import MyPosts from './MyPosts';
-import Login from '../screens/Login';
 
 const Drawer = createDrawerNavigator();
 const lbcLogo = require('../assets/lbc_logo_w_ball_gradient.png');
@@ -46,10 +45,6 @@ export default function DrawerNav() {
         <Drawer.Screen
           name="My Posts"
           component={MyPosts}
-        />
-        <Drawer.Screen
-          name="Log Out"
-          component={Login}
         />
       </Drawer.Navigator>
     </Container>
